e43d342e251b79c26d7f9e35d74705218f22a97f
amanprasadoo7 <amanprasad5455@gmail.com>
2022-11-05 21:55:39 +0530

    handle symbol, number and rerender on screen

--- calculator.js
+++ calculator.js
@@ -1,4 +1,8 @@
+// to refresh and update values
+let buffer = "0";
 
+// 5. to target the screen
+const screen = document.querySelector('.screen');
 
 // 3. Checking the values after click
 function buttonClick(value) {
@@ -9,21 +13,54 @@ function buttonClick(value) {
         // console.log(value);  numerical values
         handleNumber(value);
     }
-  }
-
-
-//   4. handeling Numbers
-function handleNumber(value) { 
-    // console.log("Number ",value);
-    
+    // 7. to update buffer
+    rerender(); 
  }
 
 
- //   5. handeling Symbol
-function handleSymbol(value) { 
-    // console.log("Symbol ",value);
+// 4. handeling Numbers
+function handleNumber(number) { 
+    // console.log("Number ",number);
+    if(buffer === "0"){
+      buffer = number; // Edge cases
+    }else{
+      buffer += number; //to update buffer
+    }
+    // console.log(buffer);
  }
 
+//  9. Handle Math Function
+function handleMath(symbol) {
+
+}
+
+// 8. handeling Symbol
+function handleSymbol(symbol) { 
+  // console.log("Symbol ",symbol);
+  switch (symbol) {
+    case "C":
+      buffer = "0"
+      // console.log("cancel");
+      break;
+    case "←":
+      // console.log("backspace");
+      if (buffer.length === 1) {
+        buffer = "0";
+      } else {
+        buffer = buffer.substring(0, buffer.length - 1);
+        // console.log(buffer);
+      }
+      break;
+    case "+":
+    case "-":
+    case "×":
+    case "÷":
+    case "=":
+      handleMath(symbol);
+      // console.log('Symbol',symbol );
+      break;
+    }
+ }
 
 // 1. Initilise function
 function init() {
@@ -34,4 +71,10 @@ function init() {
       });
   }
   
-  init(); // 2. invoking the function
+// 6. to update the buffer
+function rerender(){
+  screen.innerText = buffer;
+}
+
+// 2. invoking the function
+init(); 
