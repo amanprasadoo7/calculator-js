689e64642fb18b412c33c6e5482ddbb0b3fcafeb
amanprasadoo7 <amanprasad5455@gmail.com>
2022-11-06 20:37:01 +0530

    commit

--- calculator.js
+++ calculator.js
@@ -1,80 +1,97 @@
-// to refresh and update values
+let runningTotal = 0;
 let buffer = "0";
+let previousOperator;
+const screen = document.querySelector(".screen");
 
-// 5. to target the screen
-const screen = document.querySelector('.screen');
-
-// 3. Checking the values after click
 function buttonClick(value) {
-    if(isNaN(parseInt(value))) { // checking symbol
-        // console.log(value);
-        handleSymbol(value);
-    }else{
-        // console.log(value);  numerical values
-        handleNumber(value);
-    }
-    // 7. to update buffer
-    rerender(); 
- }
-
-
-// 4. handeling Numbers
-function handleNumber(number) { 
-    // console.log("Number ",number);
-    if(buffer === "0"){
-      buffer = number; // Edge cases
-    }else{
-      buffer += number; //to update buffer
-    }
-    // console.log(buffer);
- }
-
-//  9. Handle Math Function
-function handleMath(symbol) {
-
+  if (isNaN(parseInt(value))) {
+    handleSymbol(value);
+  } else {
+    handleNumber(value);
+  }
+  rerender();
 }
 
-// 8. handeling Symbol
-function handleSymbol(symbol) { 
-  // console.log("Symbol ",symbol);
-  switch (symbol) {
+function handleNumber(value) {
+  if (buffer === "0") {
+    buffer = value;
+  } else {
+    buffer += value;
+  }
+}
+
+function handleMath(value) {
+  if (buffer === "0") {
+    // do nothing
+    return;
+  }
+
+  const intBuffer = parseInt(buffer);
+  if (runningTotal === 0) {
+    runningTotal = intBuffer;
+  } else {
+    flushOperation(intBuffer);
+  }
+
+  previousOperator = value;
+
+  buffer = "0";
+}
+
+function flushOperation(intBuffer) {
+  if (previousOperator === "+") {
+    runningTotal += intBuffer;
+  } else if (previousOperator === "-") {
+    runningTotal -= intBuffer;
+  } else if (previousOperator === "×") {
+    runningTotal *= intBuffer;
+  } else {
+    runningTotal /= intBuffer;
+  }
+}
+
+function handleSymbol(value) {
+  switch (value) {
     case "C":
-      buffer = "0"
-      // console.log("cancel");
+      buffer = "0";
+      runningTotal = 0;
+      break;
+    case "=":
+      if (previousOperator === null) {
+        // need two numbers to do math
+        return;
+      }
+      flushOperation(parseInt(buffer));
+      previousOperator = null;
+      buffer = +runningTotal;
+      runningTotal = 0;
       break;
     case "←":
-      // console.log("backspace");
       if (buffer.length === 1) {
         buffer = "0";
       } else {
         buffer = buffer.substring(0, buffer.length - 1);
-        // console.log(buffer);
       }
       break;
     case "+":
     case "-":
     case "×":
     case "÷":
-    case "=":
-      handleMath(symbol);
-      // console.log('Symbol',symbol );
+      handleMath(value);
       break;
-    }
- }
-
-// 1. Initilise function
-function init() {
-    document
-      .querySelector(".calc-buttons") //select section
-      .addEventListener("click", function (event) { //check clicks
-        buttonClick(event.target.innerText); //send inner text to function
-      });
   }
-  
-// 6. to update the buffer
-function rerender(){
+}
+
+function rerender() {
   screen.innerText = buffer;
 }
 
-// 2. invoking the function
-init(); 
+function init() {
+  document
+    .querySelector(".calc-buttons")
+    .addEventListener("click", function (event) {
+      buttonClick(event.target.innerText);
+    });
+}
+
+init();
